7ccac26ad993e69ae97baeffc67b7829c0605508
James Putnam <jonathan.putnam86@gmail.com>
2016-09-28 15:07:07 -0600

    level1

---
+++ src/practice.php
@@ -0,0 +1,30 @@
+<?php
+  echo "Hello World"
+
+  //NUMBERS
+  $regInt = 1234;
+
+  $octNum = 01234;
+
+  $hexNum = 0xABC;
+
+  $binaryNumber = 0b1000;
+
+
+  //FLOATING POINTS/DECIMALS
+
+  $float = 1.234
+
+  $scientific = 0.1234e4;//SN
+  $scientific = 1234e-4; //negative power
+
+
+  //Constants
+
+    define('NEW_CONSTANT', "Hello new constant!");
+
+    echo NEW_CONSTANT;
+
+  var_dump($regInt);
+
+?>
